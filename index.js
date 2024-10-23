3da6df9006a425ff720b3a77ec016deca08118f0
Chibuzor Ebede <chibuzorebede@gmail.com>
2024-10-23 06:20:46 -0400

    added get_more jobs api

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const morgan = require("morgan");
-const jobService = require("./services/remote-jobs");
 const axios = require("axios");
 const cors = require("cors");
 
@@ -14,15 +13,33 @@ app.use(morgan("tiny"));
 app.use(cors());
 
 //setup base url
-const BASE_URL = `https://himalayas.app/jobs/api?limit=20&offset=0`;
+const BASE_URL = `https://himalayas.app/jobs/api?`;
 
 //app api endpoints
 
+//get initial jobs
 app.get("/job-listings", (req, res) => {
   //make request to api and send data
-  axios.get(BASE_URL).then((response) => {
-    res.json(response.data.jobs);
-  });
+  axios
+    .get(BASE_URL + `limit=20&offset=0`)
+    .then((response) => {
+      res.json(response.data.jobs);
+    })
+    .catch((err) => console.err(err));
+});
+
+//get more jobs
+app.get("/get-more", (req, res) => {
+  //create current offset and increment anytime a new request comes in (pagination)
+  let currentOffset = 40;
+  //get more data from the api
+
+  axios
+    .get(BASE_URL + `limit=20&offset=${currentOffset}`)
+    .then((response) => {
+      res.json(response.data.jobs);
+    })
+    .catch((err) => console.err(err));
 });
 
 //set app port and start the app service
