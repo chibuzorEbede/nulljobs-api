fc45d10e9896d76c3b085981adef1047b562c072
Chibuzor Ebede <chibuzorebede@gmail.com>
2024-10-31 10:06:29 -0400

    Feat:added api endpoint for geting a single job details

--- index.js
+++ index.js
@@ -42,6 +42,30 @@ app.get("/get-more", (req, res) => {
     .catch((err) => console.err(err));
 });
 
+//get full job details
+//given a job id, return the full details for the job
+app.get("/details/:jobGuid", (req, res) => {
+  const jobGuid = req.params.jobGuid;
+  let singleJobDetails = [];
+
+  //filter the api for the given job uid
+  //call the himalays api and find the job that matches the given guid then return only the details about that job
+  axios
+    .get(BASE_URL + `limit=60&offset=0`)
+    .then((response) => {
+      singleJobDetails = response.data.jobs.filter(
+        (job) => job.guid.replace(/[^\w\s]/gi, "") === jobGuid
+      );
+      //create a promise to return the single job detail
+      const jobPromise = new Promise((resolve, reject) => {
+        resolve(res.json(singleJobDetails));
+        reject("job not found.");
+      });
+      res.send(jobPromise);
+    })
+    .catch((err) => console.error(err));
+});
+
 //set app port and start the app service
 const PORT = process.env.PORT || 3002;
 
